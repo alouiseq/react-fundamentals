cfae1add5747afa0d7e2aaf21f3f793cd6261bb0
Louie <alouiseq@gmail.com>
2022-09-23 19:29:02 -0700

    complete ex6 ec2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef()
+  const [error, setError] = React.useState()
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -14,13 +15,21 @@ function UsernameForm({onSubmitUsername}) {
     onSubmitUsername(inputRef.current.value)
   }
 
+  // EC2
+  const handleChange = () => {
+    const value = inputRef.current.value
+    const isValid = value === value.toLowerCase()
+    setError(isValid ? null : 'Username must be lower case')
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        <input type="text" ref={inputRef} />
+        <input type="text" ref={inputRef} onChange={handleChange} />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={error ? true : false}>Submit</button>
+      {error && <div role="alert">{error}</div>}
     </form>
   )
 }
