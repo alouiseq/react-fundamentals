3d90a4f8ef03a1d0fb35cafdcd5a64c85c1e6b83
Louie <alouiseq@gmail.com>
2022-09-23 18:19:11 -0700

    complete ex6 ec1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,16 +4,21 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const inputRef = React.useRef()
+
   const handleSubmit = e => {
     e.preventDefault();
-    onSubmitUsername(e.target.elements[0]?.value)
+    // onSubmitUsername(e.target.elements[0]?.value)
+
+    // EC1
+    onSubmitUsername(inputRef.current.value)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        <input type="text" ref={inputRef} />
       </div>
       <button type="submit">Submit</button>
     </form>
